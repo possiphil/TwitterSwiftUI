2aed1e63b1eebd72bd51576fca1b4248528046af
Philipp Sanktjohanser <venture@mein.gmx>
2022-12-10 11:40:06 +0100

    Initial Commit

---
+++ TwitterSwiftUI/TwitterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIApp.swift
+//  TwitterSwiftUI
+//
+//  Created by Philipp Sanktjohanser on 10.12.22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
